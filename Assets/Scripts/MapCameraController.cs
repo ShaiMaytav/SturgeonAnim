a13ef7f15c00533870b06f89afccb22410132179
ShaiMaytav <shaimaytav01@gmail.com>
2023-09-28 15:50:23 +0300

    camera slide

--- Assets/Scripts/MapCameraController.cs
+++ Assets/Scripts/MapCameraController.cs
@@ -10,7 +10,9 @@ public class MapCameraController : MonoBehaviour
     [SerializeField] private float maxZoom;
     [SerializeField] private float zoomStep;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private int memorySize;
 
+    private Vector3Memory mPosMemory;
     private Vector3 followTarget;
     private Vector3 _clickPos;
     private float _minMapX, _minMapY, _maxMapX, _maxMapY;
@@ -24,6 +26,8 @@ public class MapCameraController : MonoBehaviour
         _maxMapX = mapSprite.transform.position.x + mapSprite.bounds.size.x / 2;
         _maxMapY = mapSprite.transform.position.y + mapSprite.bounds.size.y / 2;
         followTarget = mapCamera.transform.position;
+
+        mPosMemory = new Vector3Memory(memorySize);
     }
 
 
@@ -51,19 +55,28 @@ public class MapCameraController : MonoBehaviour
         }
     }
 
-    private void MoveTarget()
+    private void MoveTarget() //TBI
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             _clickPos = mapCamera.ScreenToWorldPoint(Input.mousePosition);
+            //mPosMemory.FillStack(_clickPos - mapCamera.ScreenToWorldPoint(Input.mousePosition));
         }
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            Vector3 diff = _clickPos - mapCamera.ScreenToWorldPoint(Input.mousePosition);
-
-            followTarget += diff;
+            mPosMemory.Add(_clickPos - mapCamera.ScreenToWorldPoint(Input.mousePosition));
+            Debug.Log(mPosMemory._vectors[0].magnitude);
         }
+        else
+        {
+            mPosMemory.Add(Vector3.zero);
+            Debug.Log(mPosMemory._vectors[mPosMemory._vectors.Length - 1].magnitude);
+        }
+
+        Debug.Log(mPosMemory._vectors[mPosMemory._vectors.Length - 1].magnitude);
+        followTarget += mPosMemory.GetAverageVector();
+        //Debug.Log(mPosMemory.GetAverageVector().magnitude);
     }
 
     private void CamFolllowTarget()
@@ -119,4 +132,43 @@ public class MapCameraController : MonoBehaviour
     {
         mapCamera.orthographicSize += zoomStep;
     }
+}
+
+public struct Vector3Memory
+{
+    public Vector3[] _vectors;
+
+    public Vector3Memory(int memorySize)
+    {
+        _vectors = new Vector3[memorySize];
+    }
+
+    public Vector3 GetAverageVector()
+    {
+        Vector3 sumVec = Vector3.zero;
+        foreach (var vec in _vectors)
+        {
+            sumVec += vec;
+        }
+
+        return sumVec / _vectors.Length;
+    }
+
+    public void Add(Vector3 vec)
+    {
+        for (int i = _vectors.Length - 1; i < 0; i--)
+        {
+            _vectors[i] = _vectors[i - 1];
+        }
+        _vectors[0] = vec;
+    }
+
+    public void FillStack(Vector3 _vec)
+    {
+        for (int i = 0; i < _vectors.Length; i++)
+        {
+            _vectors[i] = _vec;
+        }
+    }
+
 }
